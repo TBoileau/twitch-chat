a3f295cc853013269a8974e5c52db03e34cc30aa
TBoileau <t-boileau@email.com>
2022-02-15 18:59:29 +0100

    fix: delete fake messages

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ChatClient} from "../services/chat_client.service";
 import {Message} from "../models/Message";
-import {Badges} from "tmi.js";
 
 @Component({
   selector: 'app-chat',
@@ -18,22 +17,5 @@ export class ChatComponent implements OnInit {
     this.chatClient.newMessage.subscribe((message: Message) => {
       this.messages.push(message);
     });
-
-    let i = 0;
-
-    const interval = setInterval(() => {
-      this.messages.push(<Message>{
-        badges: <Badges>{
-          subscriber: "18"
-        },
-        content: `Message Message Message Message Message Message Message Message Message Message Message Message Message Message Message Message ${i}`,
-        username: 'test',
-        color: '#00FF00',
-      });
-      i++;
-      if (i === 10) {
-        clearInterval(interval);
-      }
-    }, 1000);
   }
 }
