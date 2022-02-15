958d56aa1cc7536b09a4612b963b42b7e346b2a7
TBoileau <t-boileau@email.com>
2022-02-15 12:04:34 +0100

    feat: commands (#7)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ChatComponent} from './chat/chat.component';
@@ -8,7 +7,8 @@ import {RouterModule} from "@angular/router";
 import {MessageComponent} from './message/message.component';
 import {AngularFireModule} from "@angular/fire/compat";
 import {environment} from "../environments/environment";
-import {AngularFireDatabaseModule} from "@angular/fire/compat/database";
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -21,7 +21,8 @@ import {AngularFireDatabaseModule} from "@angular/fire/compat/database";
       {path: 'chat', component: ChatComponent},
     ]),
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
